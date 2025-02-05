9a606f20b91dec302d9ae53815cc15c3ee869c31
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-04 21:53:38 -0700

    Change class to className

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ function Navbar() {
           </div>
           <ul
             tabIndex={0}
-            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
             <li>
               <a>Home</a>
