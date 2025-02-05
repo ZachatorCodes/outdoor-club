3cd181e9c6825682c81171573d0cb64292595c54
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-04 22:29:26 -0700

    Add routing ability

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router"
 import Navbar from "./components/Navbar"
+import Home from "./components/Home"
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   )
 }
