dc15bda0649588124c200cc4fefafab7cc2a0f93
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-04 23:48:44 -0700

    Create basic centered title div

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,13 @@
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
+    <>
+      <div className="flex justify-center items-center">
+        <div className="bg-accent p-10 m-10 rounded-box">
+          <h1 className="text-center text-5xl">FRCC Outdoor Club</h1>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
