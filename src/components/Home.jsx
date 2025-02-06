3fce30795fbb06c1b73868b08a121e9c4cc464c5
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-06 15:20:53 -0700

    Import logo as static assest

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
+import logoImg from '../assets/oc-logo.png';
 function Home() {
   return (
     <>
       <div className="flex justify-center items-center">
         <div className="flex flex-col justify-center items-center bg-[url(src/assets/mountain-image.jpg)] bg-cover bg-center w-full h-auto">
-          <img src="src/assets/oc-logo.png" className="h-100 w-auto p-10"/>
+          <img src={logoImg} className="h-100 w-auto p-10"/>
           <div className="bg-blue-100 p-10 mx-10 mb-10 rounded-box">
             <h1 className="text-center text-5xl font-bold mb-3">FRCC Outdoor Club</h1>
             <p className="text-xl text-center mt-3">THE Front Range Community College club for thoses who love the outdoors.</p>
