f014ff8b112bf18fe80b46f4d2135f0f5d089c43
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-06 15:06:38 -0700

    Center text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
           <img src="src/assets/oc-logo.png" className="h-100 w-auto p-10"/>
           <div className="bg-blue-100 p-10 mx-10 mb-10 rounded-box">
             <h1 className="text-center text-5xl font-bold mb-3">FRCC Outdoor Club</h1>
-            <p className="text-xl mt-3">THE Front Range Community College club for thoses who love the outdoors.</p>
+            <p className="text-xl text-center mt-3">THE Front Range Community College club for thoses who love the outdoors.</p>
           </div>
         </div>
       </div>
