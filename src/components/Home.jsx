bae345838b93a756e04ceeed10248a441ec7af33
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-08 00:10:45 -0700

    Convert hardcoded color names to semantic color names

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ function Home() {
       <div className="flex justify-center items-center">
         <div className="flex flex-col justify-center items-center bg-[url(/mountain-image.jpg)] bg-cover bg-center w-full h-auto">
           <img src={'/oc-logo.png'} className="h-100 w-auto p-10"/>
-          <div className="bg-blue-100 p-10 mx-10 mb-10 rounded-box">
+          <div className="bg-accent p-10 mx-10 mb-10 rounded-box">
             <h1 className="text-center text-5xl font-bold mb-3">FRCC Outdoor Club</h1>
             <p className="text-xl text-center mt-3">THE Front Range Community College club for thoses who love the outdoors.</p>
           </div>
