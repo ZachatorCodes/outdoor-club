200aa1303d6df7103b800306129e8ce48a011806
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-08 16:49:01 -0700

    Create link to trips

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Leadership from "./components/Leadership";
+import Trips from "./components/Trips";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="leadership" element={<Leadership />} />
+        <Route path="trips" element={<Trips />} />
       </Routes>
     </>
   );
