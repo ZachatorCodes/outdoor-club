38a097d81dde7cfae9013f3b9100106c586d85e1
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-07 23:59:55 -0700

    Create path to leadership component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import { Route, Routes } from "react-router"
-import Navbar from "./components/Navbar"
-import Home from "./components/Home"
-import About from "./components/About"
+import { Route, Routes } from "react-router";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Leadership from "./components/Leadership";
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="leadership" element={<Leadership />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
