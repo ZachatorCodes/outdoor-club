b67d03423ed2fb5352dcca4c0f0ba85ade4cda4f
Zach Schwartz <zachatorcodes@gmail.com>
2025-12-02 17:09:29 -0700

    Change how image is imported and rendered

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,8 +14,8 @@ function Home() {
 
   return (
       <div className="flex justify-center items-center">
-        <div className="flex flex-col justify-center items-center bg-[url(/mountain-image.jpg)] bg-cover bg-center w-full h-auto">
-          <img src={'/oc-logo.png'} className="h-100 w-auto p-10"/>
+        <div className="flex flex-col justify-center items-center bg-cover bg-center w-full h-auto" style={{backgroundImage: `url(${mtn})`}}>
+          <img src={logo} className="h-100 w-auto p-10"/>
           <div className="bg-accent p-10 mx-10 mb-10 rounded-box">
             <h1 className="text-center text-5xl font-bold mb-3">FRCC Outdoor Club</h1>
             <p className="text-xl text-center mt-3"><i>The</i> Front Range Community College club for thoses who love the outdoors.</p>
